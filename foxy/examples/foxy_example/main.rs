2ffb6852ff8b686409200aa67e4dcd8c0a9ca8f3
Gabriel Lugo <g.alexander.lugo@gmail.com>
2023-03-16 05:33:01 -0400

    Update main.rs

--- foxy/examples/foxy_example/main.rs
+++ foxy/examples/foxy_example/main.rs
@@ -1,16 +1,4 @@
-use winit::event::{Event, WindowEvent};
-use winit::event_loop::ControlFlow;
-use foxy::renderer::Renderer;
-
 #[allow(unused)]
 fn main() {
-  // let (renderer, event_loop) = Renderer::new();
-  // event_loop.run(move |event, _, control_flow| {
-  //   if let Event::WindowEvent {
-  //     event: WindowEvent::CloseRequested,
-  //     ..
-  //   } = event {
-  //     *control_flow = ControlFlow::Exit;
-  //   }
-  // })
+  
 }
